ad9aee164852f435499ac7ff54486586b140bce3
Cyb10101 <cyb10101@gmail.com>
2019-10-05 00:18:06 +0200

    Add development bar, move reload same page to development, fix environment.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,8 +5,8 @@ global.$ = global.jQuery = require('jquery');
 require('popper.js');
 require('bootstrap');
 
-require('./environment.js');
 import {Hook} from './hook.js';
 
+require('./development.js');
 require('./logic-gate.js');
 require('./about.js');
