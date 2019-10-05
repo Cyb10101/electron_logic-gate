a6f664acd1e814dcd5b2d7fa236d0fee85c9c39a
Cyb10101 <cyb10101@gmail.com>
2019-10-05 17:27:31 +0200

    Add windows check in environment.js

--- assets/js/environment.js
+++ assets/js/environment.js
@@ -2,11 +2,15 @@
 
 class Environment {
     isProduction() {
-        return global.process.env.APP_ENV || false;
+        return (global.process.env.APP_ENV || false);
     }
 
     isDevelopment() {
-        return global.process.env.APP_ENV && global.process.env.APP_ENV === 'dev';
+        return (global.process.env.APP_ENV && global.process.env.APP_ENV === 'dev');
+    }
+
+    isWindows() {
+        return (global.process.platform === 'win32'); // Even on 64 bit
     }
 }
 
