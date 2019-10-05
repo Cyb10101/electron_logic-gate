5cfc71276d62246324f2adb4254279a68427b588
Cyb10101 <cyb10101@gmail.com>
2019-10-05 17:28:14 +0200

    Update electron script, store last window position and reuse it

--- main.js
+++ main.js
@@ -1,114 +1,151 @@
-const {app, protocol, BrowserWindow, Menu} = require('electron');
+const electron = require('electron');
+const {app, BrowserWindow, Menu} = require('electron');
 const path = require('path');
+const Store = require('electron-store');
+const store = new Store();
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+/******************************************************************************/
+// Note: ECMAScript 2015 import not ready, Electron must support Node.js v12.11.1
+class Environment {
+    isProduction() {
+        return (global.process.env.APP_ENV || false);
+    }
+
+    isDevelopment() {
+        return (global.process.env.APP_ENV && global.process.env.APP_ENV === 'dev');
+    }
+
+    isWindows() {
+        return (global.process.platform === 'win32'); // Even on 64 bit
+    }
+}
+let environment = new Environment();
+/******************************************************************************/
+
 let mainWindow;
 
-// Remove Content-Security-Policy warning
-//process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
+class ElectronApp {
+    constructor() {
 
-function isDevelopment() {
-    return (global.process.env.APP_ENV && global.process.env.APP_ENV === 'dev');
-}
-
-function isWindows() {
-    return (process.platform === 'win32'); // Even on 64 bit
-}
-
-function mainWindowMenu() {
-    const mainMenuTemplate = [{
-        label: 'File',
-        submenu: [{
-            label: 'Quit',
-            accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+    }
+    mainWindowMenu() {
+        const mainMenuTemplate = [{
+            label: 'File',
+            submenu: [{
+                label: 'Quit',
+                accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click() {
+                    app.quit();
+                }
+            }]
+        }, {
+            label: 'Development',
+            submenu: [{
+                label: 'Developer Tools',
+                role: 'toggledevtools'
+            }]
+        }, {
+            label: 'Dashboard',
             click() {
-                app.quit();
+                mainWindow.loadURL('app://index.html').catch(function () {
+                    console.error('Can\'t open Dashboard');
+                });
+            }
+        }, {
+            role: 'reload',
+            accelerator: process.platform === 'darwin' ? '' : 'F5',
+        }];
+
+        // If mac, ad empty object to menu
+        if (process.platform === 'darwin') {
+            mainMenuTemplate.unshift({});
+        }
+
+        const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+        if (environment.isDevelopment()) {
+            Menu.setApplicationMenu(mainMenu);
+        } else {
+            Menu.setApplicationMenu(null);
+        }
+    }
+
+    /**
+     * If saved window bounds (x, y) is reachable then position will be returned.
+     * @param key
+     * @param gap
+     */
+    getSavedWindowBounds(key, gap = 0) {
+        let bounds = {};
+        let savedBounds = store.get(key);
+        if (savedBounds) {
+            let displays = electron.screen.getAllDisplays();
+            let externalDisplay = displays.find((display) => {
+                let x = (display.workArea.x <= savedBounds.x && savedBounds.x < (display.bounds.x + display.size.width - gap));
+                let y = (display.workArea.y <= savedBounds.y && savedBounds.y < (display.bounds.y + display.size.height - gap));
+                return x && y;
+            });
+            if (externalDisplay) {
+                bounds.x = savedBounds.x;
+                bounds.y = savedBounds.y;
             }
         }
-        ]
-    }, {
-        label: 'Development',
-        submenu: [{
-            label: 'Developer Tools',
-            role: 'toggledevtools'
-        }]
-    }, {
-        label: 'Dashboard',
-        click() {
-            mainWindow.loadURL('app://index.html');
-        }
-    }, {
-        role: 'reload',
-        accelerator: process.platform === 'darwin' ? '' : 'F5',
-    }];
-
-    // If mac, ad empty object to menu
-    if (process.platform === 'darwin') {
-        mainMenuTemplate.unshift({});
+        return bounds;
     }
 
-    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-    if (isDevelopment()) {
-        Menu.setApplicationMenu(mainMenu);
-    } else {
-        Menu.setApplicationMenu(null);
+    mainWindowCreate() {
+        mainWindow = new BrowserWindow({
+            ...this.getSavedWindowBounds('mainWindowBounds'),
+            width: 700 + (environment.isDevelopment() ? 555 : 0), // + DevTools
+            height: (environment.isDevelopment() ? 600 : 262), // Menu + Window
+            frame: true,
+            autoHideMenuBar: false,
+            resizable: true,
+            useContentSize: true,
+            icon: path.join(__dirname, 'assets/images/icons/round-corner/64x64.png'),
+            webPreferences: {
+                nodeIntegration: true
+            }
+        });
+
+        let scheme = 'app';
+        mainWindow.webContents.session.protocol.registerFileProtocol(scheme, (request, callback) => {
+            const url = request.url.substr(scheme.length + 3);
+            callback({path: path.normalize(`${__dirname}/public/${url}`)});
+        }, (error) => {
+            if (error) {
+                console.error('Failed to register protocol');
+            }
+        });
+
+        // @todo development test
+        console.log('ScaleFactor: ', electron.screen.getPrimaryDisplay().scaleFactor);
+
+        if (environment.isDevelopment()) {
+            mainWindow.webContents.openDevTools();
+        }
+
+        mainWindow.loadURL('app://index.html').catch(function () {
+            console.error('Can\'t open Dashboard');
+        });
+
+        mainWindow.on('close', () => {
+            store.set('mainWindowBounds', mainWindow.getBounds());
+        });
+
+        mainWindow.on('closed', () => {
+            mainWindow = null
+        });
     }
 }
 
-function mainWindowCreate() {
-    mainWindow = new BrowserWindow({
-        width: 700 + (isDevelopment() ? 555 : 0), // + DevTools
-        height: (isDevelopment() ? 600 : 262), // Menu + Window
-        frame: true,
-        autoHideMenuBar: false,
-        resizable: true,
-        useContentSize: true,
-        icon: path.join(__dirname, 'assets/images/icons/round-corner/64x64.png'),
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
-
-    let scheme = 'app';
-    mainWindow.webContents.session.protocol.registerFileProtocol(scheme, (request, callback) => {
-        const url = request.url.substr(scheme.length + 3);
-        callback({path: path.normalize(`${__dirname}/public/${url}`)});
-    }, (error) => {
-        if (error) {
-            console.error('Failed to register protocol');
-        }
-    });
-
-    // @todo development test
-    const {screen} = require('electron');
-    console.log('ScaleFactor: ', screen.getPrimaryDisplay().scaleFactor);
-
-    if (isDevelopment()) {
-        mainWindow.webContents.openDevTools();
-    }
-
-    mainWindow.loadURL('app://index.html');
-
-    // Emitted when the window is closed.
-    mainWindow.on('closed', () => {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = null
-    });
-}
-
+let electronApp = new ElectronApp();
 
 app.on('ready', () => {
-    mainWindowMenu();
-    mainWindowCreate();
+    electronApp.mainWindowMenu();
+    electronApp.mainWindowCreate();
 });
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
         app.quit();
     }
@@ -118,9 +155,7 @@ app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
-        createWindow();
+        electronApp.mainWindowMenu();
+        electronApp.mainWindowCreate();
     }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
